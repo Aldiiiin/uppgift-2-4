2c0e459e1619d26ba401855aa51f5a582664314e
Admin <Admin@TEB123009>
2023-09-29 14:26:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MedellonBerakning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] loner = new double[3];
+
+            // Be användaren mata in lönen för tre anställda
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Ange lön för anställd {i + 1}: ");
+                loner[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            // Beräkna medellönen
+            double medellon = (loner[0] + loner[1] + loner[2]) / 3;
+
+            // Skriv ut medellönen
+            Console.WriteLine($"Medellönen för personalen är: {medellon:F2} kr");
+        }
+    }
+}
